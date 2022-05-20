67e6ae991be1560efb8cf24f4c13818cd8bfb14f
Mathhew <suwmlee@gmail.com>
2022-05-20 18:15:23 +0800

    add test.py

--- test.py
+++ test.py
@@ -2,6 +2,40 @@
 
 from scrapinglib import search
 
-ret = search('012717_472')
-ret = search('FC2-PPV-2848294')
+proxydict = {
+    "http": "socks5h://127.0.0.1:1080",
+    "https": "socks5h://127.0.0.1:1080"
+}
+# print(search('012717_472', 'avsox'))
+# print(search('FC2-PPV-2848294', 'avsox'))
 
+# print(search('STAR-438', 'javbus', proxies=proxydict))
+# print(search('ABP-960', 'javbus', proxies=proxydict))
+# print(search('MMNT-010', 'javbus', proxies=proxydict))
+# print(search('ipx-292', 'javbus', proxies=proxydict))
+# print(search('CEMD-011', 'javbus', proxies=proxydict))
+# print(search('CJOD-278', 'javbus', proxies=proxydict))
+# print(search('BrazzersExxtra.21.02.01', 'javbus', proxies=proxydict))
+# print(search('100221_001', 'javbus', proxies=proxydict))
+# print(search('AVSW-061', 'javbus', proxies=proxydict))
+
+# print(search('RCTD-288', 'xcity', proxies=proxydict))
+# print(search('VNDS-2624', 'xcity', proxies=proxydict))
+# print(search('ABP-988', 'xcity', proxies=proxydict))
+
+# print(search('SIRO-4149', 'mgstage', proxies=proxydict))
+
+# print(search('MD0129', 'madou', proxies=proxydict))
+# print(search('TM0002', 'madou', proxies=proxydict))
+# print(search('MD0222', 'madou', proxies=proxydict))
+# print(search('MD0140-2', 'madou', proxies=proxydict))
+# print(search('MAD039', 'madou', proxies=proxydict))
+# print(search('JDMY027', 'madou', proxies=proxydict))
+
+# print(search('FC2-2903008', 'fc2', proxies=proxydict))
+# print(search('FC2-2182382', 'fc2', proxies=proxydict))
+# print(search('FC2-607854', 'fc2', proxies=proxydict))
+# print(search('FC2-2787433', 'fc2', proxies=proxydict))
+
+print(search('VJ013178', 'dlsite', proxies=proxydict))
+print(search('RJ329607', 'dlsite', proxies=proxydict))
