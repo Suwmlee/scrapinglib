67b830597f008965fcdba2322bdb549604bbce49
Mathhew <suwmlee@gmail.com>
2022-06-20 09:28:59 +0800

    CI: update

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from codecs import open
+
+requires = [
+    "lxml",
+    "mechanicalsoup",
+    "cloudscraper",
+]
+
+with open("README.md", "r", "utf-8") as f:
+    readme = f.read()
+
+setup(
+    name='scrapinglib',
+    version='0.2.4',
+    author="suwmlee",
+    author_email='suwmlee@gmail.com',
+    url='https://github.com/Suwmlee/scrapinglib',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=["scrapinglib"],
+    package_dir={'scrapinglib': 'scrapinglib'},   
+    install_requires=requires,
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/Suwmlee/scrapinglib/issues",
+        "Source": "https://github.com/Suwmlee/scrapinglib",
+    },
+)
