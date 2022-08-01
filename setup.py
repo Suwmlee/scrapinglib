283d22c65a076ddcb066c1a68c6e02d12458c941
Mathhew <suwmlee@gmail.com>
2022-08-01 18:04:45 +0800

    Bump Version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", "utf-8") as f:
 
 setup(
     name='scrapinglib',
-    version='0.2.6',
+    version='0.3.0',
     author="suwmlee",
     author_email='suwmlee@gmail.com',
     url='https://github.com/Suwmlee/scrapinglib',
