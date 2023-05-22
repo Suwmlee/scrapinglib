cf7807d3cd13ed3fd721ec1d8e17b23bf4022292
Mathhew <suwmlee@gmail.com>
2023-05-22 10:20:45 +0800

    Update parser.py

--- scrapinglib/parser.py
+++ scrapinglib/parser.py
@@ -85,7 +85,7 @@ class Parser:
         else:
             self.detailurl = self.queryNumberUrl(number)
         if not self.detailurl:
-            return None
+            return 404
         htmltree = self.getHtmlTree(self.detailurl)
         result = self.dictformat(htmltree)
         return result
@@ -210,7 +210,13 @@ class Parser:
 
     def getTags(self, htmltree) -> list:
         alls = self.getTreeAll(htmltree, self.expr_tags)
-        return [ x.strip() for x in alls if x.strip()]
+        tags = []
+        for t in alls:
+            for tag in t.strip().split(','):
+                tag = tag.strip()
+                if tag:
+                    tags.append(tag)
+        return tags
 
     def getStudio(self, htmltree):
         return self.getTreeElementbyExprs(htmltree, self.expr_studio, self.expr_studio2)
