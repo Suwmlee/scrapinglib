522a3c3af465d6a6da745c32ee131ce413fb90b5
Mathhew <Suwmlee@users.noreply.github.com>
2026-03-09 21:46:59 +0800

    fix: fc2

--- scrapinglib/sites/fc2.py
+++ scrapinglib/sites/fc2.py
@@ -12,7 +12,7 @@ class Fc2(BaseScraper):
     content_type = 'adult'
     priority = 60
 
-    expr_title = '/html/head/title/text()'
+    expr_title = '//div[@class="items_article_headerInfo"]/h3/text()'
     expr_studio = '//*[@id="top"]/div[1]/section[1]/div/section/div[2]/ul/li[3]/a/text()'
     expr_release = '//*[@id="top"]/div[1]/section[1]/div/section/div[2]/div[2]/p/text()'
     expr_runtime = "//p[@class='items_article_info']/text()"
@@ -26,12 +26,15 @@ class Fc2(BaseScraper):
         self.imagecut = 0
         self.allow_number_change = True
 
+    def updateCore(self, core):
+        self.cookies =  {'wei6H':'1','GDPRCHECK':'true'}
+
     def search(self, number):
         self.number = number.lower().replace('fc2-ppv-', '').replace('fc2-', '')
         if self.specifiedUrl:
             self.detailurl = self.specifiedUrl
         else:
-            self.detailurl = 'https://adult.contents.fc2.com/article/' + self.number + '/'
+            self.detailurl = 'https://adult.contents.fc2.com/article/' + self.number + '/?'
         self.htmlcode = self.getHtml(self.detailurl)
         if self.htmlcode == 404:
             return 404
