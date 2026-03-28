5ac24c01eef290a684bfc43308161314070e277e
Mathhew <Suwmlee@users.noreply.github.com>
2026-03-28 21:19:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r", "utf-8") as f:
 
 setup(
     name='scrapinglib',
-    version='0.12.1',
+    version='0.12.2',
     author="suwmlee",
     author_email='suwmlee@gmail.com',
     url='https://github.com/Suwmlee/scrapinglib',
